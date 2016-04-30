cce7c14d4ebac54937299bee0bb093cf4e609ce9
Bernice Chen <bc-mac@bc-mac.local>
2016-05-01 05:52:28 +1000

    allow reminder message format to be <num> (seconds|minutes|hours)

--- src/parse.py
+++ src/parse.py
@@ -1,5 +1,5 @@
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import logger
 import re
 
@@ -13,14 +13,17 @@ INFO_TIME = "time"
 # returns the remindee, date and text of reminder
 def parse_msg(j):
     string = get_message_text(j)
-    # "Remind <userid> on <23 May 2016 14:03> of <text>"
-    p = re.compile(r'(?i)remind ([^ ]+) ([^ ]+) on (.+) of (.+)')
-    m = p.match(string)
+    # "Remind <userid> on <23 May 2016 14:03> to <text>"
+    p1 = re.compile(r'(?i)remind ([^ ]+) ([^ ]+) on (.+) to (.+)')
+    # "Remind <userid> in <12 minutes> to <text>"
+    p2 = re.compile(r'(?i)remind ([^ ]+) ([^ ]+) in ([0-9]+) (seconds|minutes|hours) to (.+)')
+    m1 = p1.match(string)
+    m2 = p2.match(string)
 
     info = dict()
 
-    if m:
-        mg = m.groups()
+    if m1:
+        mg = m1.groups()
         
         info[INFO_DEST_FIRST_NAME] = mg[0]
         info[INFO_DEST_LAST_NAME] = mg[1]
@@ -30,7 +33,24 @@ def parse_msg(j):
             info[INFO_TIME] = datetime.strptime(mg[2], "%d %B %Y %H:%M") # <23 May 2016 14:03>
         except:
             # cannot parse date format so assume remind now
-            info[INFO_TIME] = datetime.datetime.now()
+            info[INFO_TIME] = datetime.now()
+        return info
+    elif m2:
+        mg = m2.groups()
+
+        info[INFO_DEST_FIRST_NAME] = mg[0]
+        info[INFO_DEST_LAST_NAME] = mg[1]
+        info[INFO_TEXT] = mg[4]
+        info[INFO_SOURCE_USER_ID] = get_source_user_id(j)
+        info[INFO_TIME] = datetime.now()
+        amount = int(mg[2])
+        if mg[3] == "seconds":
+            info[INFO_TIME] += timedelta(seconds=amount)
+        elif mg[3] == "minutes":
+            info[INFO_TIME] += timedelta(minutes=amount)
+        elif mg[3] == "hours":
+            info[INFO_TIME] += timedelta(hours=amount)
+
         return info
     else:
         print "no match!"
